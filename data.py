0550cfe33c82a09add7684296fbc7a0e948a82e6
monajemia <watbut67@gmail.com>
2024-11-23 20:33:06 +0330

    Added transforms, just normalization for now.

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import json
 import os
+from locale import normalize
+
 import numpy as np
 from torch.utils.data import Dataset
 
@@ -9,11 +11,12 @@ config_json = 'config.json'
 
 
 class CustomDataset(Dataset):
-    def __init__(self, image_dir, mask_dir, annotation_file, image_suffix=".npy"):
+    def __init__(self, image_dir, mask_dir, annotation_file, image_suffix=".npy", transforms=None):
         self.images, self.annotations = self.load_annotations(annotation_file)
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.image_suffix = image_suffix
+        self.transforms = transforms
 
     def __len__(self):
         return len(self.images)
@@ -26,7 +29,6 @@ class CustomDataset(Dataset):
         # item is index int, converting + 1 to string that is image name
         item = str(item + 1)
         if item in self.images:
-
             # Paths to image and mask
             image_path = os.path.join(
                 self.image_dir, item + self.image_suffix
@@ -51,20 +53,44 @@ class CustomDataset(Dataset):
     def read_image(self, image_path):
         return np.load(image_path)
 
+    def transform(self, image):
+        transform_func = {
+            'normalize': self.normalize
+        }
+
+        if self.transforms:
+            for transform in self.transforms:
+                image = transform_func[transform]
+
+        return image
+
+    def normalize(self, image):
+        min_val = np.min(image)
+        max_val = np.max(image)
+
+        # Prevent devision by zero
+        if min_val == max_val:
+            return np.zeros_like(image)
+
+        normalized = (image - min_val) / (max_val - min_val)
+        return normalized
+
 
 def main():
-    # Load basic config
+    # Load config from file
     with open(config_json) as f:
         config = json.load(f)
 
+    # Load params from config
     all_dir = config['dataset']['all']
+    transforms = config['dataset']['transforms']
     if config['dataset']['compressed']:
         image_suffix = '.npz'
     else:
         image_suffix = '.npy'
 
     dataset = CustomDataset(image_dir=os.path.join(all_dir, "images"), mask_dir=os.path.join(all_dir, "masks"),
-                            annotation_file=all_dir + '.json', image_suffix=image_suffix)
+                            annotation_file=all_dir + '.json', image_suffix=image_suffix, transforms=transforms)
 
 
 if __name__ == '__main__':
