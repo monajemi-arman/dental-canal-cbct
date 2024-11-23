d5369991bd960ff291fd7e5a6cb259588e2cef87
monajemia <watbut67@gmail.com>
2024-11-23 21:34:09 +0330

    load model with config params

--- model.py
+++ model.py
@@ -2,13 +2,18 @@
 from lightning import LightningModule
 from monai.networks.nets import UNet
 from torch.nn import CrossEntropyLoss
+import torch
+import json
 from torch.optim import Adam
+
+# Path to config file
+config_json = 'config.json'
 
 
 class LightningUNet(LightningModule):
-    def __init__(self):
+    def __init__(self, **unet_params):
         super().__init__()
-        self.model = UNet(dimensions=3, in_channels=1, out_channels=2)
+        self.model = UNet(**unet_params).to("cuda" if torch.cuda.is_available() else "cpu")
         self.loss_fn = CrossEntropyLoss()
 
     def training_step(self, batch, batch_idx):
@@ -28,3 +33,15 @@ class LightningUNet(LightningModule):
         return Adam(self.parameters(), lr=0.001)
 
 
+def main():
+    # Load config from file
+    with open(config_json) as f:
+        config = json.load(f)
+
+    # Load model with custom config parameters
+    unet_params = config['model']['unet']
+    model = LightningUNet(**unet_params)
+
+
+if __name__ == '__main__':
+    main()
