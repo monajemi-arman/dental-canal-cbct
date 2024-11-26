d7eb7612f56eb1e8fff3cca26d9d59151cc4d722
monajemia <watbut67@gmail.com>
2024-11-26 18:05:40 +0330

    main function

--- train.py
+++ train.py
@@ -8,56 +8,64 @@ import os
 from model import LightningUNet
 from data import CustomDataset
 
-# Load config
+# Path to config
 config_json = "config.json"
-with open("config.json") as f:
-    config = json.load(f)
 
-# Early stopping callback
-early_stop_callback = EarlyStopping(
-    monitor='val_loss',
-    patience=10,
-    mode='min'
-)
 
-# Training
-trainer = Trainer(
-    max_epochs=100,
-    accelerator='gpu',
-    callbacks=[early_stop_callback]
-)
+def main():
+    # Load config
+    with open("config.json") as f:
+        config = json.load(f)
 
-# Dataset paths
-train = config['dataset']['train']
-val = config['dataset']['val']
-images = os.path.join(config['dataset']['all'], config['dataset']['images'])
-masks = os.path.join(config['dataset']['all'], config['dataset']['masks'])
-annotation_suffix = '.json'
-transforms = config['dataset']['transforms']
-compressed = config['dataset']['compressed']
-if compressed:
-    image_suffix = '.npz'
-else:
-    image_suffix = '.npy'
+    # Early stopping callback
+    early_stop_callback = EarlyStopping(
+        monitor='val_loss',
+        patience=10,
+        mode='min'
+    )
 
-# Dataset instances
-train_dataset = CustomDataset(images, masks, train + annotation_suffix, transforms=transforms)
-val_dataset = CustomDataset(images, masks, val + annotation_suffix, transforms=transforms)
+    # Training
+    trainer = Trainer(
+        max_epochs=100,
+        accelerator='gpu',
+        callbacks=[early_stop_callback]
+    )
 
-# Dataloader
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32)
-val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32)
+    # Dataset paths
+    train = config['dataset']['train']
+    val = config['dataset']['val']
+    images = os.path.join(config['dataset']['all'], config['dataset']['images'])
+    masks = os.path.join(config['dataset']['all'], config['dataset']['masks'])
+    annotation_suffix = '.json'
+    transforms = config['dataset']['transforms']
+    compressed = config['dataset']['compressed']
+    if compressed:
+        image_suffix = '.npz'
+    else:
+        image_suffix = '.npy'
 
-# Load model with custom config parameters
-unet_params = config['model']['unet']
-model = LightningUNet(**unet_params)
+    # Dataset instances
+    train_dataset = CustomDataset(images, masks, train + annotation_suffix, transforms=transforms)
+    val_dataset = CustomDataset(images, masks, val + annotation_suffix, transforms=transforms)
 
-# Trainer
-trainer = Trainer(
-    max_epochs=config['train']['max_epochs'],
-    accelerator=config['train']['accelerator'],
-    callbacks=[early_stop_callback]
-)
+    # Dataloader
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32)
 
-# Training start
-trainer.fit(model, train_loader, val_loader)
+    # Load model with custom config parameters
+    unet_params = config['model']['unet']
+    model = LightningUNet(**unet_params)
+
+    # Trainer
+    trainer = Trainer(
+        max_epochs=config['train']['max_epochs'],
+        accelerator=config['train']['accelerator'],
+        callbacks=[early_stop_callback]
+    )
+
+    # Training start
+    trainer.fit(model, train_loader, val_loader)
+
+
+if __name__ == '__main__':
+    main()
